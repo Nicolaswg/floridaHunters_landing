8fc363a1966eece7f9c6b02f082c9868ba08dcbe
Manjorstreep <manjorstreep@gmail.com>
2024-05-18 11:38:41 -0400

    TrailingSlash

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  trailingSlash: 'always',
   integrations: [react(), tailwind({
     applyBaseStyles: false
   })],
