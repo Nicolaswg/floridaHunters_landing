4caf81d87eda20350a94a01622105713d024d6fa
Manjorstreep <manjorstreep@gmail.com>
2024-05-18 14:02:56 -0400

    Testing without baseurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: 'always',
-  base:'/landing/',
   integrations: [react(), tailwind({
     applyBaseStyles: false
   })],
