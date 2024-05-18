9abb5557538917e30f02be18e5504c3a03396e81
Manjorstreep <manjorstreep@gmail.com>
2024-05-18 13:08:33 -0400

    Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: 'always',
+  base:'/landing',
   integrations: [react(), tailwind({
     applyBaseStyles: false
   })],
